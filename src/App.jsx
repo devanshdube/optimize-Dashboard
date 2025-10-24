da4d1b0ab06ca0049c2c31c600364f1c3bd518b3
student demo <anshkanha98@gmail.com>
2025-10-24 10:00:14 +0530

    working on login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Dashboard from "./Modules/1/Dashboard";
+import SignIn from "./Modules/Auth/SignIn";
 
 function App() {
   return (
     <>
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <SignIn />
     </>
   );
 }
